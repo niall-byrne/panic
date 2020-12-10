bd5bbbc16b280616b261e2118ce9a7e825557aaa
Niall Byrne <niall@niallbyrne.ca>
2020-12-10 21:07:32 +0000

    test(KITCHEN.MODELS.TRANSACTONS.MANAGERS): create test stubs for model manager and expiry calculator

---
+++ panic/kitchen/tests/test_models_transaction_managers.py
@@ -0,0 +1,15 @@
+"""Test the Transaction Managers and Expiry Calculator."""
+
+from django.test import TestCase
+
+
+class TestExpiryCalculator(TestCase):
+
+  def test_base_test(self):
+    raise NotImplementedError("Test Incomplete")
+
+
+class TestTransactionManager(TestCase):
+
+  def test_base_test(self):
+    raise NotImplementedError("Test Incomplete")
