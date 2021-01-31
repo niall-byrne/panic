3709ad385c33d7cf1f5f660cecb81ef1e325dfc1
Niall Byrne <niall@niallbyrne.ca>
2021-01-31 21:25:39 +0000

    test(KITCHEN.TESTS): shelf view tests use fixture, test cross user perms

--- panic/kitchen/tests/test_views_shelf.py
+++ panic/kitchen/tests/test_views_shelf.py
@@ -1,7 +1,6 @@
 """Test the Shelf API."""
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from django.utils.http import urlencode
@@ -10,8 +9,17 @@ from rest_framework.test import APIClient
 
 from ..models.shelf import Shelf
 from ..serializers.shelf import ShelfSerializer
+from .fixtures.shelf import ShelfTestHarness
 
 SHELF_URL = reverse("kitchen:shelves-list")
+
+
+class AnotherUserTestHarness(ShelfTestHarness):
+
+  @classmethod
+  def create_data_hook(cls):
+    test_data2 = cls.create_dependencies(2)
+    cls.user2 = test_data2['user']
 
 
 def shelf_url_with_params(query_kwargs):
@@ -37,41 +45,18 @@ class PublicShelfTest(TestCase):
     self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class PrivateShelfTest(TestCase):
+class PrivateShelfTest(ShelfTestHarness):
   """Test the authorized Shelf API"""
 
-  def sample_shelf(self, user=None, name="Over Sink"):
-    """Create a test shelf."""
-    if user is None:
-      user = self.user
-    shelf = Shelf.objects.create(user=user, name=name)
-    self.objects.append(shelf)
-    return shelf
-
-  @classmethod
-  def setUpTestData(cls):
-    cls.objects = list()
-    cls.user = get_user_model().objects.create_user(
-        username="testuser",
-        email="test@niallbyrne.ca",
-        password="test123",
-    )
-    cls.serializer = ShelfSerializer
-    cls.fields = {"name": 255}
-
   def setUp(self):
-    self.objects = list()
+    super().setUp()
     self.client = APIClient()
-    self.client.force_authenticate(self.user)
-
-  def tearDown(self) -> None:
-    for obj in self.objects:
-      obj.delete()
+    self.client.force_authenticate(self.user1)
 
   def test_list_shelves(self):
     """Test retrieving a list of shelves."""
-    self.sample_shelf(name="Refrigerator")
-    self.sample_shelf(name="Pantry")
+    self.create_test_instance(user=self.user1, name="Refrigerator")
+    self.create_test_instance(user=self.user1, name="Pantry")
 
     res = self.client.get(SHELF_URL)
 
@@ -86,7 +71,7 @@ class PrivateShelfTest(TestCase):
     """Test retrieving a list of shelves is paginated correctly."""
     for index in range(0, 11):
       data = 'shelfname' + str(index)
-      self.sample_shelf(name=data)
+      self.create_test_instance(user=self.user1, name=data)
 
     res = self.client.get(shelf_url_with_params({"page_size": 10}))
     self.assertEqual(len(res.data['results']), 10)
@@ -97,7 +82,7 @@ class PrivateShelfTest(TestCase):
     """Test retrieving a the full list of shelves."""
     for index in range(0, 11):
       data = 'shelfname' + str(index)
-      self.sample_shelf(name=data)
+      self.create_test_instance(user=self.user1, name=data)
 
     res = self.client.get(
         shelf_url_with_params({
@@ -109,8 +94,8 @@ class PrivateShelfTest(TestCase):
 
   def test_delete_shelf(self):
     """Test deleting a shelf."""
-    delete = self.sample_shelf(name="Refrigerator")
-    self.sample_shelf(name="Pantry")
+    delete = self.create_test_instance(user=self.user1, name="Refrigerator")
+    self.create_test_instance(user=self.user1, name="Pantry")
 
     res_delete = self.client.delete(SHELF_URL + str(delete.id) + '/')
     res_get = self.client.get(SHELF_URL)
@@ -134,3 +119,55 @@ class PrivateShelfTest(TestCase):
     assert len(shelves) == 1
     self.assertEqual(res.status_code, status.HTTP_201_CREATED)
     self.assertEqual(shelves[0].name, data['name'])
+
+
+class PrivateShelfTestAnotherUser(AnotherUserTestHarness):
+  """Test the authorized Shelf API from Another User"""
+
+  def setUp(self):
+    super().setUp()
+    self.client = APIClient()
+    self.client.force_authenticate(self.user2)
+
+  def test_list_shelves(self):
+    """Test retrieving a list of shelves."""
+    self.create_test_instance(user=self.user1, name="Refrigerator")
+    self.create_test_instance(user=self.user1, name="Pantry")
+
+    res = self.client.get(SHELF_URL)
+
+    self.assertEqual(res.status_code, status.HTTP_200_OK)
+    self.assertEqual(res.data['results'], [])
+
+  def test_list_shelves_paginated_correctly(self):
+    """Test retrieving a list of shelves is paginated correctly."""
+    for index in range(0, 11):
+      data = 'shelfname' + str(index)
+      self.create_test_instance(user=self.user1, name=data)
+
+    res = self.client.get(shelf_url_with_params({"page_size": 10}))
+    self.assertEqual(len(res.data['results']), 0)
+    self.assertIsNone(res.data['next'])
+    self.assertIsNone(res.data['previous'])
+
+  def test_list_shelves_paginated_overidden_correctly(self):
+    """Test retrieving a the full list of shelves."""
+    for index in range(0, 11):
+      data = 'shelfname' + str(index)
+      self.create_test_instance(user=self.user1, name=data)
+
+    res = self.client.get(
+        shelf_url_with_params({
+            "page_size": 10,
+            settings.PAGINATION_OVERRIDE_PARAM: "true"
+        })
+    )
+    self.assertEqual(len(res.data), 0)
+
+  def test_delete_shelf(self):
+    """Test deleting a shelf."""
+    delete = self.create_test_instance(user=self.user1, name="Refrigerator")
+    self.create_test_instance(user=self.user1, name="Pantry")
+
+    res_delete = self.client.delete(SHELF_URL + str(delete.id) + '/')
+    self.assertEqual(res_delete.status_code, status.HTTP_403_FORBIDDEN)
