3b75fb3a56d964debb869912f621235c2abe31fb
Niall Byrne <niall@niallbyrne.ca>
2021-01-31 21:40:18 +0000

    test(KITCHEN.TESTS): store view tests use fixture, test cross user perms

--- panic/kitchen/tests/test_views_store.py
+++ panic/kitchen/tests/test_views_store.py
@@ -1,7 +1,6 @@
 """Test the Store API."""
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from django.utils.http import urlencode
@@ -10,8 +9,17 @@ from rest_framework.test import APIClient
 
 from ..models.store import Store
 from ..serializers.store import StoreSerializer
+from .fixtures.store import StoreTestHarness
 
 STORE_URL = reverse("kitchen:stores-list")
+
+
+class AnotherUserTestHarness(StoreTestHarness):
+
+  @classmethod
+  def create_data_hook(cls):
+    test_data2 = cls.create_dependencies(2)
+    cls.user2 = test_data2['user']
 
 
 def store_url_with_params(query_kwargs):
@@ -37,41 +45,18 @@ class PublicStoreTest(TestCase):
     self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class PrivateStoreTest(TestCase):
+class PrivateStoreTest(StoreTestHarness):
   """Test the authorized Store API"""
 
-  def sample_store(self, user=None, name="No Frills"):
-    """Create a test store."""
-    if user is None:
-      user = self.user
-    store = Store.objects.create(user=user, name=name)
-    self.objects.append(store)
-    return store
-
-  @classmethod
-  def setUpTestData(cls):
-    cls.objects = list()
-    cls.user = get_user_model().objects.create_user(
-        username="testuser",
-        email="test@niallbyrne.ca",
-        password="test123",
-    )
-    cls.serializer = StoreSerializer
-    cls.fields = {"name": 255}
-
   def setUp(self):
-    self.objects = list()
+    super().setUp()
     self.client = APIClient()
-    self.client.force_authenticate(self.user)
-
-  def tearDown(self) -> None:
-    for obj in self.objects:
-      obj.delete()
+    self.client.force_authenticate(self.user1)
 
   def test_list_stores(self):
     """Test retrieving a list of stores."""
-    self.sample_store(name="No Frills")
-    self.sample_store(name="Loblaws")
+    self.create_test_instance(user=self.user1, name="No Frills")
+    self.create_test_instance(user=self.user1, name="Loblaws")
 
     res = self.client.get(STORE_URL)
 
@@ -86,7 +71,7 @@ class PrivateStoreTest(TestCase):
     """Test that retrieving a list of stores is limited correctly."""
     for index in range(0, 11):
       data = "storename" + str(index)
-      self.sample_store(name=data)
+      self.create_test_instance(user=self.user1, name=data)
 
     res = self.client.get(store_url_with_params({"page_size": 10}))
     self.assertEqual(len(res.data['results']), 10)
@@ -97,7 +82,7 @@ class PrivateStoreTest(TestCase):
     """Test retrieving a the full list of stores."""
     for index in range(0, 11):
       data = 'storesname' + str(index)
-      self.sample_store(name=data)
+      self.create_test_instance(user=self.user1, name=data)
 
     res = self.client.get(
         store_url_with_params({
@@ -109,8 +94,8 @@ class PrivateStoreTest(TestCase):
 
   def test_delete_store(self):
     """Test deleting a store."""
-    delete = self.sample_store(name="A&P")
-    self.sample_store(name="Beckers")
+    delete = self.create_test_instance(user=self.user1, name="A&P")
+    self.create_test_instance(user=self.user1, name="Beckers")
 
     res_delete = self.client.delete(STORE_URL + str(delete.id) + '/')
     res_get = self.client.get(STORE_URL)
@@ -134,3 +119,55 @@ class PrivateStoreTest(TestCase):
     assert len(shelves) == 1
     self.assertEqual(res.status_code, status.HTTP_201_CREATED)
     self.assertEqual(shelves[0].name, data['name'])
+
+
+class PrivateStoreTestAnotherUser(AnotherUserTestHarness):
+  """Test the authorized Store API from Another User"""
+
+  def setUp(self):
+    super().setUp()
+    self.client = APIClient()
+    self.client.force_authenticate(self.user2)
+
+  def test_list_stores(self):
+    """Test retrieving a list of stores."""
+    self.create_test_instance(user=self.user1, name="No Frills")
+    self.create_test_instance(user=self.user1, name="Loblaws")
+
+    res = self.client.get(STORE_URL)
+
+    self.assertEqual(res.status_code, status.HTTP_200_OK)
+    self.assertEqual(res.data['results'], [])
+
+  def test_list_stores_paginated_correctly(self):
+    """Test that retrieving a list of stores is limited correctly."""
+    for index in range(0, 11):
+      data = "storename" + str(index)
+      self.create_test_instance(user=self.user1, name=data)
+
+    res = self.client.get(store_url_with_params({"page_size": 10}))
+    self.assertEqual(len(res.data['results']), 0)
+    self.assertIsNone(res.data['next'])
+    self.assertIsNone(res.data['previous'])
+
+  def test_list_stores_paginated_overidden_correctly(self):
+    """Test retrieving a the full list of stores."""
+    for index in range(0, 11):
+      data = 'storesname' + str(index)
+      self.create_test_instance(user=self.user1, name=data)
+
+    res = self.client.get(
+        store_url_with_params({
+            "page_size": 10,
+            settings.PAGINATION_OVERRIDE_PARAM: "true"
+        })
+    )
+    self.assertEqual(len(res.data), 0)
+
+  def test_delete_store(self):
+    """Test deleting a store."""
+    delete = self.create_test_instance(user=self.user1, name="A&P")
+    self.create_test_instance(user=self.user1, name="Beckers")
+
+    res_delete = self.client.delete(STORE_URL + str(delete.id) + '/')
+    self.assertEqual(res_delete.status_code, status.HTTP_403_FORBIDDEN)
